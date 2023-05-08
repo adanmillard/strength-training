88e0ad7cda301df6c6ab8f187c4eb1efc0a9f06c
Adan Millard <adanmillard@gmail.com>
2023-05-08 15:13:01 +1200

    created function to work out percentage based lifts for each week.

--- src/components/531/FiveThreeOneProgram.tsx
+++ src/components/531/FiveThreeOneProgram.tsx
@@ -1,36 +1,83 @@
 import { PercentageTable } from "../PercentageTable/PercentageTable";
 import { useState } from "react";
 
+type LiftInputs = {
+  deadlift: number;
+  squat: number;
+  bench: number;
+  overhead: number;
+};
+
+const initialLiftInputs: LiftInputs = {
+  deadlift: 0,
+  squat: 0,
+  overhead: 0,
+  bench: 0,
+};
+
+type PercentArray = number[];
+
+type Week = {
+  percentArray: PercentArray;
+  liftPerc: number[][];
+};
+
+const defaultWeeks: Week[] = [
+  { percentArray: [0.65, 0.75, 0.85, 0.7], liftPerc: [] },
+  { percentArray: [0.7, 0.8, 0.9, 0.75], liftPerc: [] },
+  { percentArray: [0.75, 0.85, 0.95, 0.8], liftPerc: [] },
+  { percentArray: [0.4, 0.5, 0.6, 0.45], liftPerc: [] },
+];
+
 export const FiveThreeOneProgram = () => {
-  const [squat, setSquat] = useState<number>(0);
-  const [deadlift, setDeadlift] = useState<number>(0);
-  const [bench, setBench] = useState<number>(0);
-  const [overhead, setOverhead] = useState<number>(0);
+  const [liftInputs, setLiftInputs] = useState<LiftInputs>(initialLiftInputs);
+  const [week, setWeek] = useState(defaultWeeks);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
-    switch (name) {
-      case "deadlift":
-        setDeadlift(Number(value));
-        break;
-      case "bench":
-        setBench(Number(value));
-        break;
-      case "overhead":
-        setOverhead(Number(value));
-        break;
-      case "squat":
-        setSquat(Number(value));
-        break;
-      default:
-        break;
-    }
+    setLiftInputs((prevInputs) => ({
+      ...prevInputs,
+      [name]: parseFloat(value),
+    }));
   };
 
-  const handleSubmit = () => {
-    console.log(deadlift, squat, bench, overhead);
+  const calculateWeekValues = (week: Week, liftName: keyof LiftInputs) => {
+    const liftValue = liftInputs[liftName];
+    return week.percentArray.map((percent) => percent * liftValue);
   };
 
+  const handleCalculateClick = () => {
+    const newWeeks = week.map((week) => ({
+      percentArray: week.percentArray,
+      liftPerc: [
+        calculateWeekValues(week, "deadlift"),
+        calculateWeekValues(week, "squat"),
+        calculateWeekValues(week, "bench"),
+        calculateWeekValues(week, "overhead"),
+      ],
+    }));
+    setWeek(newWeeks);
+    newWeeks.forEach((lift) => {
+      const liftValues = lift.liftPerc[0]; // get the array of values
+      console.log(`[${liftValues.join(", ")}]`);
+    });
+  };
+
+  /*
+  const handleCalculateClick = () => {
+    const newWeeks = week.map((week) => ({
+      percentArray: week.percentArray,
+      liftPerc: [
+        calculateWeekValues(week, "deadlift"),
+        calculateWeekValues(week, "squat"),
+        calculateWeekValues(week, "bench"),
+        calculateWeekValues(week, "overhead"),
+      ],
+    }));
+    setWeek(newWeeks);
+    console.log(newWeeks);
+  };
+*/
   return (
     <div>
       <div>
@@ -46,31 +93,31 @@ export const FiveThreeOneProgram = () => {
         <input
           type="number"
           name="deadlift"
-          value={deadlift}
+          value={liftInputs.deadlift}
           onChange={handleInputChange}
         />
         <label>1RM Squat</label>
         <input
           type="number"
           name="squat"
-          value={squat}
+          value={liftInputs.squat}
           onChange={handleInputChange}
         />
         <label>1RM Bench Press</label>
         <input
           type="number"
           name="bench"
-          value={bench}
+          value={liftInputs.bench}
           onChange={handleInputChange}
         />
         <label>1RM Overhead Press</label>
         <input
           type="number"
           name="overhead"
-          value={overhead}
+          value={liftInputs.overhead}
           onChange={handleInputChange}
         />
-        <button onClick={handleSubmit}>Go</button>
+        <button onClick={handleCalculateClick}>Go</button>
       </div>
       <div>
         <PercentageTable />
