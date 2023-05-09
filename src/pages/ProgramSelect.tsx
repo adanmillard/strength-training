40a588f9d3e157d3c7eb244e6a58bca97c40259b
Adan Millard <adanmillard@gmail.com>
2023-05-10 11:36:42 +1200

    error handling for 0 value inputs

--- src/pages/ProgramSelect.tsx
+++ src/pages/ProgramSelect.tsx
@@ -13,6 +13,12 @@ export const ProgramSelect = () => {
       <div>
         <h1>Select your program:</h1>
         <button onClick={handleNavigate}>5/3/1</button>
+        <button>Training 2</button>
+        <button>Training 3</button>
+        <button>Training 4</button>
+        <button>Training 5</button>
+        <button>Single Lifts</button>
+        <button>Accessory Exercises</button>
       </div>
     </div>
   );
