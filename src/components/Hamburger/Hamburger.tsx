992531f17ba78a7967be8cbfe0c8187bc3b63ff9
Adan Millard <adanmillard@gmail.com>
2023-05-21 12:15:04 +1200

    created hamburger menu for navigation within the site.

--- src/components/Hamburger/Hamburger.tsx
+++ src/components/Hamburger/Hamburger.tsx
@@ -11,24 +11,27 @@ export const Hamburger = () => {
   ];
 
   return (
-    <>
-      <div>
-        <div
-          onClick={() => setOpen(!open)}
-          className="text-3xl absolute right-8 top-6 cursor-pointer"
-        >
-          <FontAwesomeIcon icon={open ? faClose : faBoxOpen} size="xs" />
-        </div>
-        <ul className="">
-          {navItems.map((item, i) => {
-            return (
-              <li key={i}>
-                <a href={item.link}>{item.navName}</a>
-              </li>
-            );
-          })}
-        </ul>
+    <div>
+      <div
+        onClick={() => setOpen(!open)}
+        className="text-3xl cursor-pointer z-10 mr-4 mt-4 fixed right-0 top-0"
+      >
+        <FontAwesomeIcon icon={open ? faClose : faBoxOpen} size="xs" />
       </div>
-    </>
+      <div className={open ? "flex w-full h-full flex-row-reverse" : "hidden"}>
+        <span className="w-full h-full bg-transparent-black fixed"></span>
+        <div className="bg-white h-full w-72 fixed">
+          <ul className=" relative ml-4 mt-4 top-10 ">
+            {navItems.map((item, i) => {
+              return (
+                <li key={i} className="p-2 m-2">
+                  <a href={item.link}>{item.navName}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 };
