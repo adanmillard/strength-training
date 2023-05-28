8efc72af85ab129ee56d0fbb04860f6ff20232ef
Adan Millard <adanmillard@gmail.com>
2023-05-29 10:45:10 +1200

    styled accessory model.

--- src/components/Accessories/Accessories.tsx
+++ src/components/Accessories/Accessories.tsx
@@ -9,11 +9,12 @@ export const Accessories: React.FC<Props> = ({
 }) => {
   return (
     <div className=" bg-white rounded-md shadow-lg h-3/4 w-3/4 absolute p-5 border">
-      <div>
-        <button onClick={() => setAccessoryLifts(!accessoryLifts)}>
-          &#10005;
-        </button>
-      </div>
+      <button
+        className="absolute top-4 right-4 text-lg"
+        onClick={() => setAccessoryLifts(!accessoryLifts)}
+      >
+        &#10005;
+      </button>
       <div className="flex flex-col items-center justify-evenly w-full h-full">
         <h1>Accessory Exercises </h1>
         <div>
@@ -26,31 +27,31 @@ export const Accessories: React.FC<Props> = ({
             3 and reps of 10, pick a weight where you are almost failing on the
             10th rep.
           </p>
-          <div className="flex w-full justify-around">
-            <div className="mb-8 mt-4">
-              <h2>Deadlift:</h2>
-              <p>Straight Leg Deadlifts</p>
-              <p>Bent Over Rows</p>
-              <p>Lat Pull Downs</p>
-            </div>
-            <div className="mb-8 mt-4">
-              <h2>Squat:</h2>
-              <p>Leg Press</p>
-              <p>Lunges</p>
-              <p>Lying Leg Curls</p>
-            </div>
-            <div className="mb-8 mt-4">
-              <h2>Bench Press:</h2>
-              <p>Incline Dumbbell Press</p>
-              <p>Body Weight Dips</p>
-              <p>Skullcrushers</p>
-            </div>
-            <div className="mb-8 mt-4">
-              <h2>Overhead Press:</h2>
-              <p>Seated Dumbbell Press</p>
-              <p>Lateral Raises</p>
-              <p>Tricep Extensions</p>
-            </div>
+        </div>
+        <div className="flex w-full justify-around">
+          <div className="mb-8 mt-4">
+            <h2>Deadlift:</h2>
+            <p>Straight Leg Deadlifts</p>
+            <p>Bent Over Rows</p>
+            <p>Lat Pull Downs</p>
+          </div>
+          <div className="mb-8 mt-4">
+            <h2>Squat:</h2>
+            <p>Leg Press</p>
+            <p>Lunges</p>
+            <p>Lying Leg Curls</p>
+          </div>
+          <div className="mb-8 mt-4">
+            <h2>Bench Press:</h2>
+            <p>Incline Dumbbell Press</p>
+            <p>Body Weight Dips</p>
+            <p>Skullcrushers</p>
+          </div>
+          <div className="mb-8 mt-4">
+            <h2>Overhead Press:</h2>
+            <p>Seated Dumbbell Press</p>
+            <p>Lateral Raises</p>
+            <p>Tricep Extensions</p>
           </div>
         </div>
       </div>
