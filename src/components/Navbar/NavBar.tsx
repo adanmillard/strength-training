a21630ebcb3f190ef56039aead214c2fcb6d57e8
Adan Millard <adanmillard@gmail.com>
2023-05-29 09:07:44 +1200

    changed homepage layout.

--- src/components/Navbar/NavBar.tsx
+++ src/components/Navbar/NavBar.tsx
@@ -27,7 +27,7 @@ export const NavBar = () => {
     <>
       <nav className="flex justify-between h-20 items-center shadow-md fixed w-full">
         <div className="ml-4">
-          <h2>Strength Training</h2>
+          <h2>Training Buddy</h2>
         </div>
         <div
           onClick={() => setOpen(!open)}
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -5,8 +5,24 @@ export const Homepage = () => {
     <>
       <NavBar />
       <div className="flex flex-col justify-center h-screen items-center">
-        <h1>Strength Training</h1>
-        <h1>Helper</h1>
+        <h1>
+          <b>Training Buddy</b>
+        </h1>
+        <p className="my-2">
+          Thought about starting the gym but didn't know how?
+        </p>
+        <p className="my-2">
+          Look no further, Training Buddy is here to help, with beginner
+          friendly workouts to help get you into the gym and achieve your goals!
+        </p>
+        <p className="my-2">
+          With Strength Training, Muscle Building and Weight Loss. Training
+          Buddy is here to help you{" "}
+          <a href="/program-select" className="text-blue-500">
+            get started
+          </a>
+          .
+        </p>
       </div>
     </>
   );
